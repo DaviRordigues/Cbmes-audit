71d0d011fa5cdc1100fec084f82191cc98cd3a61
T01764 <pedro.rodrigues@taking.com.br>
2024-08-11 00:40:09 -0300

    feat: FIX

--- src/main/java/br/es/gov/cb/cbmesaudit/services/imps/AuditServiceImpl.java
+++ src/main/java/br/es/gov/cb/cbmesaudit/services/imps/AuditServiceImpl.java
@@ -1,8 +1,8 @@
 package br.es.gov.cb.cbmesaudit.services.imps;
 
+import br.es.gov.cb.cbmesaudit.dtos.PagedAuditDTO;
 import br.es.gov.cb.cbmesaudit.dtos.RequestAuditDTO;
 import br.es.gov.cb.cbmesaudit.dtos.ResponseAuditDTO;
-import br.es.gov.cb.cbmesaudit.dtos.PagedAuditDTO;
 import br.es.gov.cb.cbmesaudit.entities.AuditEntity;
 import br.es.gov.cb.cbmesaudit.mapper.AuditMapper;
 import br.es.gov.cb.cbmesaudit.repositorys.AuditRepository;
@@ -15,29 +15,28 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class AuditServiceImpl implements AuditService {
-
+	
 	private final AuditRepository auditRepository;
-
+	
 	@Override
 	public void create(RequestAuditDTO requestAuditDTO) {
 		auditRepository.save(AuditMapper.createAuditEntityFromDTO(requestAuditDTO));
 	}
-
+	
 	@Override
 	public PagedAuditDTO findAll(Pageable pageable, RequestAuditDTO requestAuditDTO) {
 		Specification<AuditEntity> spec = AuditSpecification.getByFilter(requestAuditDTO);
 		Page<AuditEntity> page = auditRepository.findAll(spec, pageable);
-
+		
 		List<ResponseAuditDTO> audits = page.getContent()
 				.stream()
 				.map(AuditMapper::createAuditDTOFromEntity)
-				.collect(Collectors.toList());
-
+				.toList();
+		
 		return PagedAuditDTO.builder()
 				.audits(audits)
 				.currentPage(page.getNumber())
--- src/main/java/br/es/gov/cb/cbmesaudit/specification/AuditSpecification.java
+++ src/main/java/br/es/gov/cb/cbmesaudit/specification/AuditSpecification.java
@@ -3,6 +3,7 @@ package br.es.gov.cb.cbmesaudit.specification;
 import br.es.gov.cb.cbmesaudit.dtos.RequestAuditDTO;
 import br.es.gov.cb.cbmesaudit.entities.AuditEntity;
 import jakarta.persistence.criteria.Predicate;
+import lombok.AllArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.time.LocalDateTime;
@@ -11,30 +12,32 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@AllArgsConstructor
 public class AuditSpecification {
-
-    public static Specification<AuditEntity> getByFilter(RequestAuditDTO requestAuditDTO) {
-        return (root, query, criteriaBuilder) -> {
-            List<Predicate> predicates = new ArrayList<>();
-
-            if (requestAuditDTO.getAuditedUser() != null) {
-                predicates.add(criteriaBuilder.equal(root.get("auditedUser"), requestAuditDTO.getAuditedUser()));
-            }
-
-            if (requestAuditDTO.getSourceApp() != null) {
-                predicates.add(criteriaBuilder.equal(root.get("sourceApp"), requestAuditDTO.getSourceApp()));
-            }
-
-            if (requestAuditDTO.getCreationDate() != null) {
-                LocalDateTime startOfDay = requestAuditDTO.getCreationDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().atStartOfDay();
-                LocalDateTime endOfDay = startOfDay.plusDays(1).minusSeconds(1);
-                predicates.add(criteriaBuilder.between(root.get("creationDate"),
-                        Date.from(startOfDay.atZone(ZoneId.systemDefault()).toInstant()),
-                        Date.from(endOfDay.atZone(ZoneId.systemDefault()).toInstant())));
-            }
-
-            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
-        };
-    }
-
+	
+	public static Specification<AuditEntity> getByFilter(RequestAuditDTO requestAuditDTO) {
+		return (root, query, criteriaBuilder) -> {
+			List<Predicate> predicates = new ArrayList<>();
+			
+			//TODO: Aqui é melhor utilizar o Objects.nonNull() para evitar null pointer exception
+			if (requestAuditDTO.getAuditedUser() != null) {
+				predicates.add(criteriaBuilder.equal(root.get("auditedUser"), requestAuditDTO.getAuditedUser()));
+			}
+			
+			if (requestAuditDTO.getSourceApp() != null) {
+				predicates.add(criteriaBuilder.equal(root.get("sourceApp"), requestAuditDTO.getSourceApp()));
+			}
+			
+			if (requestAuditDTO.getCreationDate() != null) {
+				LocalDateTime startOfDay = requestAuditDTO.getCreationDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().atStartOfDay();
+				LocalDateTime endOfDay = startOfDay.plusDays(1).minusSeconds(1);
+				predicates.add(criteriaBuilder.between(root.get("creationDate"),
+						Date.from(startOfDay.atZone(ZoneId.systemDefault()).toInstant()),
+						Date.from(endOfDay.atZone(ZoneId.systemDefault()).toInstant())));
+			}
+			
+			return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+		};
+	}
+	
 }
